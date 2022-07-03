d3dd1f63edae448916fa9912a0cfb448c959bb58
Marvelous Emoghene <marvelous.emoghene@gmail.com>
2022-07-03 08:44:45 +0100

    add payload object

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+
+const trx = {
+    "ID": 1308,
+    "Amount": 12580,
+    "Currency": "NGN",
+    "CustomerEmail": "anon8@customers.io",
+    "SplitInfo": [
+        {
+            "SplitType": "FLAT",
+            "SplitValue": 45,
+            "SplitEntityId": "LNPYACC0019"
+        },
+        {
+            "SplitType": "RATIO",
+            "SplitValue": 3,
+            "SplitEntityId": "LNPYACC0011"
+        },
+        {
+            "SplitType": "PERCENTAGE",
+            "SplitValue": 3,
+            "SplitEntityId": "LNPYACC0015"
+        }
+    ]
+}
+
+const app = express();
+app.use(express.json());
+
