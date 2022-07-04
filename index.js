b71bd9cbd111c5820c4059f9b72f34175c581589
Marvelous Emoghene <marvelous.emoghene@gmail.com>
2022-07-04 12:09:36 +0100

    remove sample payload add final computation

--- index.js
+++ index.js
@@ -1,79 +1,57 @@
 const express = require('express');
 
-// sample payload
-const trx = {
-    "ID": 13092,
-    "Amount": 4500,
-    "Currency": "NGN",
-    "CustomerEmail": "anon8@customers.io",
-    "SplitInfo": [
-        {
-            "SplitType": "FLAT",
-            "SplitValue": 450,
-            "SplitEntityId": "LNPYACC0019"
-        },
-        {
-            "SplitType": "RATIO",
-            "SplitValue": 3,
-            "SplitEntityId": "LNPYACC0011"
-        },
-        {
-            "SplitType": "PERCENTAGE",
-            "SplitValue": 3,
-            "SplitEntityId": "LNPYACC0015"
-        },
-        {
-            "SplitType": "RATIO",
-            "SplitValue": 2,
-            "SplitEntityId": "LNPYACC0016"
-        },
-        {
-            "SplitType": "FLAT",
-            "SplitValue": 2450,
-            "SplitEntityId": "LNPYACC0029"
-        },
-        {
-            "SplitType": "PERCENTAGE",
-            "SplitValue": 10,
-            "SplitEntityId": "LNPYACC0215"
-        }
-    ]
-}
-
 const app = express();
 app.use(express.json());
 
 // -- ENDPOINT -- //
 app.post('/split-payments/compute', (req, res) => {
-    const { ID, Amount, Currency, CustomerEmail, SplitInfo } = req.body;
+    let { ID, Amount, Currency, CustomerEmail, SplitInfo } = req.body;
     // check if all required fields are present
     if (!ID || !Amount || !Currency || !CustomerEmail || !SplitInfo) {
         res.status(400).send('Missing required fields');
     }
-    else {
-        // for all FLAT SplitTypes, compute Amount - SplitValue at each index
-        // using the difference of this value as the amount for the next SplitValue
-        // and the remaining amount as the amount for the next SplitValue
-        let remainingAmount = Amount;
-        for (let i = 0; i < SplitInfo.length; i++) {
-            if (SplitInfo[i].SplitType === 'FLAT') {
-                SplitInfo[i].SplitValue = remainingAmount - SplitInfo[i].SplitValue;
-                remainingAmount = SplitInfo[i].SplitValue;
-            }
-            // else if SplitType is PERCENTAGE, compute the percentage of the remaining amount
-            // and use this value as the amount for the next SplitValue
-            else if (SplitInfo[i].SplitType === 'PERCENTAGE') {
-                SplitInfo[i].SplitValue = remainingAmount - SplitInfo[i].SplitValue / 100;
-                remainingAmount = SplitInfo[i].SplitValue;
-            }
-        }
+    // check if SplitInfo is an empty or has more than 20 entities
+    if (SplitInfo.length === 0 || SplitInfo.length > 20) {
+        res.status(400).send('SplitInfo is empty, please provide at least one split');
     }
+    // compute in this order:
+    // 1. assign a variable to the amount of the transaction
+    let totalAmount = Amount;
+    // 2. for all FLAT splits, subtract the splitvalue from total amount
+    SplitInfo.forEach(split => {
+        if (split.SplitType === 'FLAT') {
+            totalAmount -= split.SplitValue;
+        }
+    })
+    // 3. for all PERCENTAGE splits, subtract the percentage of the splitvalue from the total amount
+    SplitInfo.forEach(split => {
+        if (split.SplitType === 'PERCENTAGE') {
+            totalAmount -= (split.SplitValue / 100) * totalAmount;
+        }
+    })
+    // 4. for all RATIO splits
+    // i. compute the total ratio
+    let totalRatio = 0;
+    SplitInfo.forEach(split => {
+        if (split.SplitType === 'RATIO') {
+            totalRatio += split.SplitValue;
+        }
+    })
+    // ii. for all RATIO splits, multiply the ratio of the splitvalue and total ratio with the same total amount
+    // then subtract the result from the consecutive total amount
+    SplitInfo.forEach(split => {
+        if (split.SplitType === 'RATIO') {
+            totalAmount -= (split.SplitValue / totalRatio) * totalAmount;
+            totalAmount -= totalAmount;
+        }
+    })
+    res.status(200).json(totalAmount);
 })
 
-
-// -- LISTEN ENDPOINT -- //
+// -- ROOT -- //
 app.get('/', function (req, res) {
-    res.send('Hello World')
-  })
-  
-app.listen(3000)
+    res.send('Server is running on port 3000');
+})
+
+// -- START SERVER -- //
+app.listen(3000);
